1a25a619cdacaf752a8b2e65f9de36f5bedaf8e5
mnapthine <mnapthine@gmail.com>
2020-11-24 13:05:52 +0000

    Decent solution for starters on meta

--- src/components/EditPreview/EditPreview.tsx
+++ src/components/EditPreview/EditPreview.tsx
@@ -33,77 +33,75 @@ import {
 
 const EditPreview = ({ className: classNameProp, ...rest }: any) => {
   const slider = useRef<HTMLDivElement>(null);
-  const [sliderOffset, setSliderOffset] = useState(0);
+  // const [sliderOffset, setSliderOffset] = useState(0);
   const [sliderScrolled, setSliderScrolled] = useState(false);
-  const [scrollDirection, setScrollDirection] = useState("forwards");
+  // const [scrollDirection, setScrollDirection] = useState("forwards");
 
-  const setSliderShuntPosition = _throttle(
-    () => {
-      if (slider.current !== null) {
-        const currentOffset = slider.current.scrollTop;
-        // console.log(currentOffset);
-        // const direction = currentOffset > boardOffsetX ? "forwards" : "back";
-        // console.log("in3", scrollDirection);
-        setSliderOffset(currentOffset);
-        // setScrollDirection(direction);
+  // const setSliderShuntPosition = _throttle(
+  //   () => {
+  //     if (slider.current !== null) {
+  //       const currentOffset = slider.current.scrollTop;
+  //       setSliderOffset(currentOffset);
+  //     }
+  //   },
+  //   10000,
+  //   { trailing: true }
+  // );
 
-        // const currentOffset = slider.current.scrollLeft;
-        // const direction = currentOffset > boardOffsetX ? "forwards" : "back";
-        // setBoardOffsetX(slider.current.scrollLeft);
-        // setScrollDirection(direction);
-      }
-    },
-    1,
-    { trailing: true }
-  );
+  // const setOffset = () => {
+  //   if (slider.current !== null) {
+  //     // const currentOffset = slider.current.scrollTop;
+  //     // setSliderOffset(slider.current.scrollTop);
+  //     if (!sliderScrolled && slider.current.scrollTop > 0) {
+  //       setSliderScrolled(true);
+  //     } else {
+  //       setSliderScrolled(false);
+  //     }
+  //   }
+  // };
 
   useEffect(
     // Figure out where the scroll is going and set the class to position the item.
     () => {
-      setSliderShuntPosition.cancel();
+      // setSliderShuntPosition.cancel();
       // TODO: remove listener on unmount...
       slider.current !== null &&
-        slider.current.addEventListener("scroll", setSliderShuntPosition);
+        slider.current.addEventListener("scroll", () => {
+          if (slider.current !== null) {
+            console.log(slider.current.scrollTop);
+            // const currentOffset = slider.current.scrollTop;
+            // setSliderOffset(slider.current.scrollTop);
+            if (slider.current.scrollTop > 300) {
+              setSliderScrolled(true);
+            } else if (slider.current.scrollTop < 100) {
+              setSliderScrolled(false);
+            }
+          }
+        });
     },
-    [setSliderShuntPosition]
+    [sliderScrolled]
   );
 
-  useEffect(
-    // Figure out where the scroll is going and set the class to position the item.
-    () => {
-      // console.log("in", slider.current);
-      if (slider.current !== null) {
-        const currentOffset = slider.current.scrollTop;
-        // console.log("in2", currentOffset, sliderOffset);
-        if (
-          // scrollDirection === "forwards" &&
-          sliderOffset > 35 &&
-          !sliderScrolled
-        ) {
-          setSliderScrolled(true);
-          console.log("in");
-          setSliderOffset(sliderOffset);
-        }
-      }
-      // if (sliderOffset > 100 && !sliderScrolled) {
-      //   setSliderScrolled(true);
-      //   console.log("in");
-      //   setSliderOffset(sliderOffset);
-      // }
-      if (
-        sliderOffset === 0 &&
-        // scrollDirection === "backwards" &&
-        sliderScrolled
-      ) {
-        // console.log("in2");
-        setSliderScrolled(false);
-        // setSliderOffset(sliderOffset);
-      }
+  // useEffect(
+  //   // Figure out where the scroll is going and set the class to position the item.
+  //   () => {
+  //     // console.log("in", slider.current);
+  //     if (slider.current !== null) {
+  //       console.log("effect?");
+  //       if (!sliderScrolled && sliderOffset > 80) {
+  //         setSliderScrolled(true);
+  //         // setSliderOffset(sliderOffset);
+  //       }
+  //       if (sliderOffset === 0 && sliderScrolled) {
+  //         // setSliderScrolled(false);
+  //         // setSliderOffset(sliderOffset);
+  //       }
+  //     }
 
-      // sliderOffset < 100 && sliderScrolled && setSliderScrolled(false);
-    },
-    [sliderOffset, sliderScrolled]
-  );
+  //     // sliderOffset < 100 && sliderScrolled && setSliderScrolled(false);
+  //   },
+  //   [sliderOffset, sliderScrolled]
+  // );
 
   return (
     // <DefaultLayout>
@@ -112,13 +110,18 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
         <header className={classes.header}>Meuu</header>
         <div className={classes.internalGrid}>
           <div className={classes.content}>
-            <div className={classes.scroll} ref={slider}>
+            <div
+              className={classnames(classes.scroll, {
+                [classes.metaBlockMini]: sliderScrolled
+              })}
+              ref={slider}
+            >
               <div
                 className={classnames(classes.contentBlock, classes.metaBlock, {
-                  [classes.metaBlockMini]: sliderScrolled
+                  // [classes.metaBlockMini]: sliderScrolled
                 })}
               >
-                <Label>Meta data</Label>
+                {/* <Label>Meta data</Label> */}
                 <Grid variant={2} className={classes.metaGrid}>
                   <div className={classes.tdi}>
                     <img
@@ -169,11 +172,11 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
                   </div>
                 </Grid>
               </div>
-
+              <div className={classes.spacer} />
               <div className={classes.contentBlock}>
                 <Label>Page Title</Label>
                 <InputText
-                  id="blah2"
+                  id="ptitle"
                   labelVisuallyHidden
                   placeholder="Title"
                   label={"Page title"}
@@ -184,7 +187,7 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
               <div className={classes.contentBlock}>
                 <Label>Banner</Label>
                 <InputText
-                  id="blah2"
+                  id="btitle"
                   labelVisuallyHidden
                   placeholder="Title"
                   label={"Page title"}
@@ -192,7 +195,7 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
                   vol={6}
                 />
                 <InputText
-                  id="blah2"
+                  id="bdesc"
                   labelVisuallyHidden
                   placeholder="Description"
                   label={"Page title"}
@@ -203,9 +206,9 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
               </div>
 
               <div className={classes.contentBlock}>
-                <Label>Body copydfg45a</Label>
+                <Label>Body copy</Label>
                 <InputText
-                  id="blah2"
+                  id="body1"
                   labelVisuallyHidden
                   placeholder="Content"
                   label={"Page title"}
@@ -213,11 +216,12 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
                   rows={3}
                   vol={3}
                 />
+                {/* <textarea></textarea> */}
               </div>
               <div className={classes.contentBlock}>
                 <Label>Reference list</Label>
                 <InputText
-                  id="blah2"
+                  id="reftitle"
                   labelVisuallyHidden
                   placeholder="Title"
                   label={"Page title"}
@@ -225,7 +229,7 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
                   vol={5}
                 />
                 <InputText
-                  id="blah2"
+                  id="refdecs"
                   labelVisuallyHidden
                   placeholder="Description"
                   label={"Page title"}
@@ -280,7 +284,39 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
             />
             {/* <Button>On</Button> */}
           </div>
+
+          {/* <g id="globe-wire"><path d="M8 0c-4.418 0-8 3.582-8 8s3.582 8 8 8c4.418 0 8-3.582 8-8s-3.582-8-8-8zM14.8 9.5c0 0.5-0.7 0.66-2 1 0.124-0.589 0.206-1.277 0.229-1.98l2.001-0.020c0 0.36-0.080 0.5-0.16 1v0zM1.2 9.5v0c-0.1-0.5-0.15-0.64-0.2-1h2c0.024 0.723 0.106 1.411 0.244 2.079-1.344-0.419-2.044-0.579-2.044-1.079zM1.2 6.5c0-0.5 0.7-0.66 2-1-0.115 0.594-0.187 1.284-0.2 1.989l-2 0.011c0-0.36 0.080-0.5 0.16-1v0zM8.5 5c1.13 0.013 2.226 0.107 3.298 0.277 0.047 0.643 0.165 1.41 0.201 2.199l-3.499 0.025v-2.5zM8.5 4v-2.94c1.17 0.27 2.2 1.47 2.84 3.15-0.836-0.116-1.819-0.192-2.817-0.21zM7.5 1.060v2.94c-1.017 0.015-2.001 0.087-2.968 0.214 0.768-1.684 1.798-2.884 2.968-3.154zM7.5 5v2.5h-3.5c0.031-0.806 0.142-1.571 0.326-2.307 0.932-0.080 2.035-0.177 3.158-0.193zM4 8.5h3.5v2.5c-1.13-0.013-2.226-0.107-3.298-0.277-0.047-0.643-0.165-1.41-0.201-2.199zM7.5 12v2.94c-1.17-0.27-2.2-1.47-2.84-3.15 0.836 0.116 1.819 0.192 2.817 0.21zM8.5 14.94v-2.94c1.017-0.015 2.001-0.087 2.968-0.214-0.768 1.684-1.798 2.884-2.968 3.154zM8.5 11v-2.5h3.5c-0.031 0.806-0.142 1.571-0.326 2.307-0.932 0.080-2.035 0.177-3.158 0.193zM15 7.5h-2c-0.024-0.723-0.106-1.411-0.244-2.079 1.354 0.399 2.014 0.559 2.014 1.079v0c0.13 0.5 0.18 0.64 0.23 1zM14.3 4.91c-0.506-0.204-1.106-0.38-1.726-0.5-0.361-1.019-0.809-1.898-1.389-2.672 1.355 0.726 2.413 1.811 3.067 3.131zM4.84 1.76c-0.568 0.752-1.019 1.631-1.305 2.581-0.699 0.189-1.299 0.365-1.874 0.593 0.751-1.39 1.823-2.475 3.139-3.156zM1.73 11.090c0.506 0.204 1.106 0.38 1.726 0.5 0.361 1.019 0.809 1.898 1.389 2.672-1.367-0.722-2.436-1.807-3.097-3.131zM11.17 14.24c0.564-0.753 1.012-1.631 1.295-2.581 0.699-0.189 1.299-0.365 1.874-0.593-0.751 1.39-1.823 2.475-3.139 3.156z"></path></g>
+<g id="globe"><path d="M8 0c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zM13.2 5.3c0.4 0 0.7 0.3 1.1 0.3-0.3 0.4-1.6 0.4-2-0.1 0.3-0.1 0.5-0.2 0.9-0.2zM1 8c0-0.4 0-0.8 0.1-1.3 0.1 0 0.2 0.1 0.3 0.1 0 0 0.1 0.1 0.1 0.2 0 0.3 0.3 0.5 0.5 0.5 0.8 0.1 1.1 0.8 1.8 1 0.2 0.1 0.1 0.3 0 0.5-0.6 0.8-0.1 1.4 0.4 1.9 0.5 0.4 0.5 0.8 0.6 1.4 0 0.7 0.1 1.5 0.4 2.2-2.5-1.2-4.2-3.6-4.2-6.5zM8 15c-0.7 0-1.5-0.1-2.1-0.3-0.1-0.2-0.1-0.4 0-0.6 0.4-0.8 0.8-1.5 1.3-2.2 0.2-0.2 0.4-0.4 0.4-0.7 0-0.2 0.1-0.5 0.2-0.7 0.3-0.5 0.2-0.8-0.2-0.9-0.8-0.2-1.2-0.9-1.8-1.2s-1.2-0.5-1.7-0.2c-0.2 0.1-0.5 0.2-0.5-0.1 0-0.4-0.5-0.7-0.4-1.1-0.1 0-0.2 0-0.3 0.1s-0.2 0.2-0.4 0.1c-0.2-0.2-0.1-0.4-0.1-0.6 0.1-0.2 0.2-0.3 0.4-0.4 0.4-0.1 0.8-0.1 1 0.4 0.3-0.9 0.9-1.4 1.5-1.8 0 0 0.8-0.7 0.9-0.7s0.2 0.2 0.4 0.3c0.2 0 0.3 0 0.3-0.2 0.1-0.5-0.2-1.1-0.6-1.2 0-0.1 0.1-0.1 0.1-0.1 0.3-0.1 0.7-0.3 0.6-0.6 0-0.4-0.4-0.6-0.8-0.6-0.2 0-0.4 0-0.6 0.1-0.4 0.2-0.9 0.4-1.5 0.4 1.1-0.8 2.5-1.2 3.9-1.2 0.3 0 0.5 0 0.8 0-0.6 0.1-1.2 0.3-1.6 0.5 0.6 0.1 0.7 0.4 0.5 0.9-0.1 0.2 0 0.4 0.2 0.5s0.4 0.1 0.5-0.1c0.2-0.3 0.6-0.4 0.9-0.5 0.4-0.1 0.7-0.3 1-0.7 0-0.1 0.1-0.1 0.2-0.2 0.6 0.2 1.2 0.6 1.8 1-0.1 0-0.1 0.1-0.2 0.1-0.2 0.2-0.5 0.3-0.2 0.7 0.1 0.2 0 0.3-0.1 0.4-0.2 0.1-0.3 0-0.4-0.1s-0.1-0.3-0.4-0.3c-0.1 0.2-0.4 0.3-0.4 0.6 0.5 0 0.4 0.4 0.5 0.7-0.6 0.1-0.8 0.4-0.5 0.9 0.1 0.2-0.1 0.3-0.2 0.4-0.4 0.6-0.8 1-0.8 1.7s0.5 1.4 1.3 1.3c0.9-0.1 0.9-0.1 1.2 0.7 0 0.1 0.1 0.2 0.1 0.3 0.1 0.2 0.2 0.4 0.1 0.6-0.3 0.8 0.1 1.4 0.4 2 0.1 0.2 0.2 0.3 0.3 0.4-1.3 1.4-3 2.2-5 2.2z"></path></g> */}
+          {/* <g id="user-card"><path d="M15 3v10h-14v-10h14zM16 2h-16v12h16v-12z"></path><path d="M8 5h6v1h-6v-1z"></path><path d="M8 7h6v1h-6v-1z"></path><path d="M8 9h3v1h-3v-1z"></path><path d="M5.4 7h-0.4v-0.1c0.6-0.2 1-0.8 1-1.4 0-0.8-0.7-1.5-1.5-1.5s-1.5 0.7-1.5 1.5c0 0.7 0.4 1.2 1 1.4v0.1h-0.4c-0.9 0-1.6 0.7-1.6 1.6v2.4h5v-2.4c0-0.9-0.7-1.6-1.6-1.6z"></path></g> */}
           <div className={classes.previewModes}>
+            <InputSelection
+              id="web"
+              inputPos="bottom"
+              className={classes.radioIcon}
+              name="viewPort"
+              label={
+                <Icon alt="On the web">
+                  {/* {< id="glasses">} */}
+                  {/* <path d="M15.5 7h-0.5c-0.1 0-0.1 0-0.2 0-0.4-1.2-1.5-2-2.8-2s-2.4 0.9-2.8 2.1c-0.3-0.4-0.7-0.6-1.2-0.6s-0.9 0.2-1.2 0.6c-0.4-1.2-1.5-2.1-2.8-2.1s-2.4 0.9-2.8 2c-0.1 0-0.1 0-0.2 0h-0.5c-0.3 0-0.5 0.2-0.5 0.5s0.2 0.5 0.5 0.5h0.5c0 1.7 1.3 3 3 3 1.5 0 2.7-1.1 3-2.5 0 0 0 0 0 0 0.3 0 0.5-0.2 0.5-0.5s0.2-0.5 0.5-0.5 0.5 0.2 0.5 0.5c0 0.3 0.2 0.5 0.5 0.5 0 0 0 0 0 0 0.2 1.4 1.5 2.5 3 2.5 1.7 0 3-1.3 3-3h0.5c0.3 0 0.5-0.2 0.5-0.5s-0.2-0.5-0.5-0.5zM4 10c-1.1 0-2-0.9-2-2s0.9-2 2-2 2 0.9 2 2-0.9 2-2 2zM12 10c-1.1 0-2-0.9-2-2s0.9-2 2-2 2 0.9 2 2-0.9 2-2 2z"></path> */}
+                  {/* wire globe */}
+                  {/* <path d="M8 0c-4.418 0-8 3.582-8 8s3.582 8 8 8c4.418 0 8-3.582 8-8s-3.582-8-8-8zM14.8 9.5c0 0.5-0.7 0.66-2 1 0.124-0.589 0.206-1.277 0.229-1.98l2.001-0.020c0 0.36-0.080 0.5-0.16 1v0zM1.2 9.5v0c-0.1-0.5-0.15-0.64-0.2-1h2c0.024 0.723 0.106 1.411 0.244 2.079-1.344-0.419-2.044-0.579-2.044-1.079zM1.2 6.5c0-0.5 0.7-0.66 2-1-0.115 0.594-0.187 1.284-0.2 1.989l-2 0.011c0-0.36 0.080-0.5 0.16-1v0zM8.5 5c1.13 0.013 2.226 0.107 3.298 0.277 0.047 0.643 0.165 1.41 0.201 2.199l-3.499 0.025v-2.5zM8.5 4v-2.94c1.17 0.27 2.2 1.47 2.84 3.15-0.836-0.116-1.819-0.192-2.817-0.21zM7.5 1.060v2.94c-1.017 0.015-2.001 0.087-2.968 0.214 0.768-1.684 1.798-2.884 2.968-3.154zM7.5 5v2.5h-3.5c0.031-0.806 0.142-1.571 0.326-2.307 0.932-0.080 2.035-0.177 3.158-0.193zM4 8.5h3.5v2.5c-1.13-0.013-2.226-0.107-3.298-0.277-0.047-0.643-0.165-1.41-0.201-2.199zM7.5 12v2.94c-1.17-0.27-2.2-1.47-2.84-3.15 0.836 0.116 1.819 0.192 2.817 0.21zM8.5 14.94v-2.94c1.017-0.015 2.001-0.087 2.968-0.214-0.768 1.684-1.798 2.884-2.968 3.154zM8.5 11v-2.5h3.5c-0.031 0.806-0.142 1.571-0.326 2.307-0.932 0.080-2.035 0.177-3.158 0.193zM15 7.5h-2c-0.024-0.723-0.106-1.411-0.244-2.079 1.354 0.399 2.014 0.559 2.014 1.079v0c0.13 0.5 0.18 0.64 0.23 1zM14.3 4.91c-0.506-0.204-1.106-0.38-1.726-0.5-0.361-1.019-0.809-1.898-1.389-2.672 1.355 0.726 2.413 1.811 3.067 3.131zM4.84 1.76c-0.568 0.752-1.019 1.631-1.305 2.581-0.699 0.189-1.299 0.365-1.874 0.593 0.751-1.39 1.823-2.475 3.139-3.156zM1.73 11.090c0.506 0.204 1.106 0.38 1.726 0.5 0.361 1.019 0.809 1.898 1.389 2.672-1.367-0.722-2.436-1.807-3.097-3.131zM11.17 14.24c0.564-0.753 1.012-1.631 1.295-2.581 0.699-0.189 1.299-0.365 1.874-0.593-0.751 1.39-1.823 2.475-3.139 3.156z"></path> */}
+                  {/* <path d="M15 3v10h-14v-10h14zM16 2h-16v12h16v-12z"></path>
+                  <path d="M8 5h6v1h-6v-1z"></path>
+                  <path d="M8 7h6v1h-6v-1z"></path>
+                  <path d="M8 9h3v1h-3v-1z"></path>
+                  <path d="M5.4 7h-0.4v-0.1c0.6-0.2 1-0.8 1-1.4 0-0.8-0.7-1.5-1.5-1.5s-1.5 0.7-1.5 1.5c0 0.7 0.4 1.2 1 1.4v0.1h-0.4c-0.9 0-1.6 0.7-1.6 1.6v2.4h5v-2.4c0-0.9-0.7-1.6-1.6-1.6z"></path> */}
+                  <g id="health-card">
+                    <path d="M15 3v10h-14v-10h14zM16 2h-16v12h16v-12z"></path>
+                    <path d="M9 5h5v1h-5v-1z"></path>
+                    <path d="M9 7h5v1h-5v-1z"></path>
+                    <path d="M9 9h2v1h-2v-1z"></path>
+                    {/* <path d="M6.5 5c0 0 0 0 0 0-0.6 0-1.1 0.6-1.5 1-0.4-0.4-0.9-1-1.5-1 0 0 0 0 0 0-1.5 0-2.1 1.9-1 2.9l2.5 2.1 2.5-2.1c1.1-1 0.5-2.9-1-2.9z"></path> */}
+                  </g>
+                </Icon>
+              }
+              type="radio"
+              vol={1}
+            />
             <InputSelection
               id="laptop"
               inputPos="bottom"
@@ -323,6 +359,25 @@ const EditPreview = ({ className: classNameProp, ...rest }: any) => {
               type="radio"
               vol={1}
             />
+            <InputSelection
+              id="full"
+              inputPos="bottom"
+              className={classes.radioIcon}
+              name="viewPort"
+              label={
+                <Icon alt="Mobile">
+                  {/* mobile */}
+                  <path d="M5.3 6.7l1.4-1.4-3-3 1.3-1.3h-4v4l1.3-1.3z"></path>
+                  <path d="M6.7 10.7l-1.4-1.4-3 3-1.3-1.3v4h4l-1.3-1.3z"></path>
+                  <path d="M10.7 9.3l-1.4 1.4 3 3-1.3 1.3h4v-4l-1.3 1.3z"></path>
+                  <path d="M11 1l1.3 1.3-3 3 1.4 1.4 3-3 1.3 1.3v-4z"></path>
+                </Icon>
+              }
+              type="radio"
+              vol={1}
+            />
+
+            {/* <g id="expand-full"><path d="M5.3 6.7l1.4-1.4-3-3 1.3-1.3h-4v4l1.3-1.3z"></path><path d="M6.7 10.7l-1.4-1.4-3 3-1.3-1.3v4h4l-1.3-1.3z"></path><path d="M10.7 9.3l-1.4 1.4 3 3-1.3 1.3h4v-4l-1.3 1.3z"></path><path d="M11 1l1.3 1.3-3 3 1.4 1.4 3-3 1.3 1.3v-4z"></path></g> */}
           </div>
           <div className={classes.previewFullScreen}>
             <Button>Off</Button>
